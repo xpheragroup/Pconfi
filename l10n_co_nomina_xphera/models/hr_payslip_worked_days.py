a1aff85c1bf1464187b02ce658e5dbf47d8d38e5
Bradember Beltran <bybeltranc@unal.edu.co>
2022-05-23 17:21:54 -0500

    First commit modul Nomina

---
+++ l10n_co_nomina_xphera/models/hr_payslip_worked_days.py
@@ -0,0 +1,19 @@
+from datetime import date, datetime, timedelta
+from odoo import api, fields, models
+
+class HrPayslipWorkedDays(models.Model):
+    _inherit = 'hr.payslip.worked_days'
+
+    prima_semestre_1 = fields.Boolean(string='Pago de Primera Prima')
+    prima_semestre_2 = fields.Boolean(string='Pago de Segunda Prima')
+    cesantias = fields.Boolean(string='Pago de Cesantias')
+    vacaciones = fields.Boolean(string='Pago de Vacaciones')
+    
+    numero_horas_diurnas_ordinarias = fields.Float(string='Horas Diurnas Ordinales')
+    numero_horas_nocturnas_ordinarias = fields.Float(string='Horas Nocuturna Ordinales')
+    numero_horas_diurnas_festivas = fields.Float(string='Horas Diurnas Festivas')
+    numero_horas_nocturnas_festivas = fields.Float(string='Horas Nocuturna Festivas')
+    numero_horas_extras_ordinarias_diurnas = fields.Float(string='Horas Extra Ordinarias Diurnas')
+    numero_horas_extras_ordinarias_nocturnas = fields.Float(string='Horas Extra Ordinarias Nocturnas')
+    numero_horas_extras_festivas_diurnas = fields.Float(string='Horas Extra Diurnas Festivas')
+    numero_horas_extras_festivas_nocturnas = fields.Float(string='Horas Extra Nocturnas Festivas')
